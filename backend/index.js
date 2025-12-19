aac224291a0c31de0147465709af17d15a41f184
Jean <210258884+LizzardBlizzardDev@users.noreply.github.com>
2025-12-19 08:28:24 -0600

    Configurar CORS para producción

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://chat-delta-steel.vercel.app",
+        methods: ["GET", "POST"]
     }
 });
 
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://chat-backend-ksx4.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
